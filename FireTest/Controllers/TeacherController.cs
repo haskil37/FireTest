4e03135d9f54a574ce9f5ecda23d03fea4c4b71e
Дмитрий <haskil@live.ru>
2016-11-21 23:23:51 +0300

    edit test 50%

--- FireTest/Controllers/TeacherController.cs
+++ FireTest/Controllers/TeacherController.cs
@@ -16,7 +16,7 @@ namespace FireTest.Controllers
         ApplicationDbContext dbContext = new ApplicationDbContext();
         public ActionResult Index()
         {
-            var tests = dbContext.TeacherTests.ToList().Where(u => u.TeacherId == User.Identity.GetUserId());
+            var tests = dbContext.TeacherTests.ToList().Where(u => u.TeacherId == User.Identity.GetUserId()).Where(u=>!string.IsNullOrEmpty(u.NameTest));
 
             return View(tests);
         }
@@ -43,8 +43,33 @@ namespace FireTest.Controllers
                 testDetails.Questions.Add(dbContext.Questions.Find(Convert.ToInt32(item)));
             return View(testDetails);
         }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TeacherTest teacherTest = dbContext.TeacherTests.Find(id);
+            if (teacherTest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacherTest);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TeacherTest teacherTest = dbContext.TeacherTests.Find(id);
+            dbContext.TeacherTests.Remove(teacherTest);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public ActionResult CreateTest()
         {
+            Session.Clear();
+
             string userId = User.Identity.GetUserId();
             var result = dbContext.TeacherTests
                 .Where(u => u.TeacherId == userId)
@@ -63,20 +88,27 @@ namespace FireTest.Controllers
             Session["Test"] = test.Id;
             return View();
         }
-        public PartialViewResult CreateTestAjax(string currentFilter, string searchString, int? page, string NameTest, int? Subjects, string Tags, int? Page, int? submitButton)
+        public PartialViewResult CreateTestAjax(string currentFilter, string searchString, int? page, string NameTest, int? Subjects, string Tags, int? submitButton)
         {
-            string userId = User.Identity.GetUserId();
-            int sessionId = (int)Session["Test"]; //Берм ид теста
-            TeacherTest test = dbContext.TeacherTests.Find(sessionId);
+            if ((int)Session["Subjects"] != Subjects || (string)Session["Tags"] != Tags)
+                page = 1;
+            Session["Subjects"] = Subjects;
+            Session["Tags"] = Tags;
 
+            string userId = User.Identity.GetUserId();
+            int sessionId = (int)Session["Test"]; //Берем ид теста
+            TeacherTest test = dbContext.TeacherTests.Find(sessionId);
+            ViewBag.NameTest = test.NameTest;
             if (!string.IsNullOrEmpty(NameTest))
             {
                 if (test != null && test.NameTest != NameTest)
                 {
                     test.NameTest = NameTest; //Сохраняем название если оно изменилось
+                    ViewBag.NameTest = NameTest;
                     dbContext.SaveChanges();
                 }
             }
+
             List<string> subjects = new List<string>();
             if (test != null && test.Questions != null)
                 subjects = test.Questions.Split('|').ToList();
@@ -109,6 +141,7 @@ namespace FireTest.Controllers
                     test.Questions = newSubjects;
                     if (string.IsNullOrEmpty(test.NameTest))
                         test.NameTest = "Без названия " + sessionId;
+                    ViewBag.NameTest = test.NameTest;
                 }
                 else
                 {
@@ -118,11 +151,12 @@ namespace FireTest.Controllers
                     newtest.Questions = newSubjects;
                     dbContext.TeacherTests.Add(newtest);
                     Session["Test"] = newtest.Id;
+                    ViewBag.NameTest = newtest.NameTest;
                 }
                 dbContext.SaveChanges();
             }
 
-            if (searchString != null)
+            if (!string.IsNullOrEmpty(searchString))
                 page = 1;
             else
                 searchString = currentFilter;
@@ -226,12 +260,105 @@ namespace FireTest.Controllers
 
             model = model.OrderBy(u => u.Name).ToList();
             int pageSize = 10;
-            int pageNumber = (Page ?? 1);
-            ViewBag.Page = pageNumber;
-            ViewBag.Name = NameTest;
+            int pageNumber = (page ?? 1);
+            ViewBag.page = pageNumber;
 
             return PartialView(model.ToPagedList(pageNumber, pageSize));
         }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TeacherTest teacherTest = dbContext.TeacherTests.Find(id);
+            if (teacherTest == null)
+            {
+                return HttpNotFound();
+            }
+            TeacherTestDetails testDetails = new TeacherTestDetails();
+            testDetails.Id = teacherTest.Id;
+            testDetails.NameTest = teacherTest.NameTest;
+            List<string> questions = teacherTest.Questions.Split('|').ToList();
+            testDetails.Questions = new List<Question>();
+            foreach (string item in questions)
+                testDetails.Questions.Add(dbContext.Questions.Find(Convert.ToInt32(item)));
+            Session["IdTest"] = id;
+            return View(testDetails);
+        }
+        public PartialViewResult EditOld()
+        {
+            return PartialView();
+        }
+        public PartialViewResult EditOldAjax(string currentFilter, string searchString, int? page, string NameTest, int? submitButton)
+        {
+            string userId = User.Identity.GetUserId();
+            int IdTest = (int)Session["IdTest"];
+            TeacherTest test = dbContext.TeacherTests.Find(IdTest);
+
+            if (!string.IsNullOrEmpty(NameTest))
+            {
+                if (test != null && test.NameTest != NameTest)
+                {
+                    test.NameTest = NameTest; //Сохраняем название если оно изменилось
+                    dbContext.SaveChanges();
+                }
+            }
+            TeacherTestDetails testDetails = new TeacherTestDetails();
+            ViewBag.NameTest = test.NameTest;
+            List<int> questions = test.Questions.Split('|').Select(int.Parse).ToList();
+            if (submitButton != null)
+            {
+                int index = questions.IndexOf(submitButton.Value);
+                questions.RemoveAt(index);
+                string newQuestions = "";
+                foreach (int item in questions)
+                {
+                    if (newQuestions.Length != 0)
+                        newQuestions = newQuestions + "|" + item;
+                    else
+                        newQuestions = item.ToString();
+                }
+                test.Questions = newQuestions;
+                dbContext.SaveChanges();
+            }
+
+            if (!string.IsNullOrEmpty(searchString))
+                page = 1;
+            else
+                searchString = currentFilter;
+            ViewBag.CurrentFilter = searchString;
+
+            var questionsOld = dbContext.Questions.Where(u => questions.Contains(u.Id)).ToList();
+            List<SubjectAccess> model = new List<SubjectAccess>(); //Использую это т.к. надо точно такие же поля
+            foreach (var item in questionsOld)
+            {
+                SubjectAccess subject = new SubjectAccess();
+                if (!String.IsNullOrEmpty(searchString) && item.QuestionText.ToLower().Contains(searchString.ToLower()))
+                {
+                    subject.Id = item.Id;
+                    subject.Name = item.QuestionText;
+                    model.Add(subject);
+                }
+                if (String.IsNullOrEmpty(searchString))
+                {
+                    subject.Id = item.Id;
+                    subject.Name = item.QuestionText;
+                    model.Add(subject);
+                }
+            }
+
+            model = model.OrderBy(u => u.Name).ToList();
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            if (model.ToPagedList(pageNumber, pageSize).PageCount < page)
+                pageNumber = model.ToPagedList(pageNumber, pageSize).PageCount;
+            ViewBag.page = pageNumber;
+
+            return PartialView(model.ToPagedList(pageNumber, pageSize));
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
