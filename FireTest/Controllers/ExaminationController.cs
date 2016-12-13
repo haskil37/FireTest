f73fc199ae5a60d7e3bbf3419cc7f7ec3a03ec9c
Дмитрий <@DESKTOP>
2016-12-13 19:51:04 +0300

    new loading indicator

---
+++ FireTest/Controllers/ExaminationController.cs
@@ -0,0 +1,19 @@
+﻿using FireTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FireTest.Controllers
+{
+    [Authorize]
+    public class ExaminationController : Controller
+    {
+        ApplicationDbContext dbContext = new ApplicationDbContext();
+        public ActionResult Access()
+        {
+            return View();
+        }
+    }
+}
